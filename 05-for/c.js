599150ed9d9d81b6bebb46eee0be3a4d49550972
Nicolas Andres Calvo <nico.calvo2014@gmail.com>
2020-10-24 20:39:38 -0300

    ex-5-c

---
+++ 05-for/c.js
@@ -0,0 +1,8 @@
+var words = ["firstWord", "secondWord", "thirdWord", "fourWord", "fifthWord"];
+var sentence = "";
+
+for (i = 0; i < words.length; i++) {
+    sentence += " " + words[i];
+}
+
+console.log(sentence);
