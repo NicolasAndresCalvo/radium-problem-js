a3a63b2b264b74fc22056ec5056bae3fdac0b621
Nicolas Andres Calvo <nico.calvo2014@gmail.com>
2020-10-24 20:57:08 -0300

    ex-6-a

---
+++ 06-functions/a.js
@@ -0,0 +1,27 @@
+var a = 1;
+var b = 2;
+
+function sum (a, b) {
+    return a + b;
+}
+
+var sumFunction = sum(a,b);
+
+console.log(sumFunction);
+
+
+// var a = 1;
+// var b = 2;
+
+// function sum (a, b) {
+//     return a + b;
+// }
+
+// console.log(sum(a, b));
+
+
+// function sum (a, b) {
+//     return 1 + 2;
+// }
+
+// console.log(sum());
