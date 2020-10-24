b440f2bd131eaf7a0ec2d3bde0c1fe8a514fd751
Nicolas Andres Calvo <nico.calvo2014@gmail.com>
2020-10-24 02:41:36 -0300

    ex-4-b

---
+++ 04-if-else/b.js
@@ -0,0 +1,17 @@
+var Age = 20;
+
+if (Age < 2) {
+    console.log("Bebe");
+} else if (Age >= 2 && Age <= 12) {
+    console.log("Niño");
+} else if (Age >= 13 && Age <= 19) {
+    console.log("Adolecente");
+} else if (Age >= 20 && Age <= 30) {
+    console.log("Joven");
+} else if (Age >= 31 && Age <= 60) {
+    console.log("Adulto");
+} else if (Age >= 61 && Age <= 75) {
+    console.log("Adulto mayor");
+} else if (Age > 75) {
+    console.log("Anciano");
+} 
