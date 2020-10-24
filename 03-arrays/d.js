8eefe7ea378c4c496b922d50132fd1ccd2771f8f
Nicolas Andres Calvo <nico.calvo2014@gmail.com>
2020-10-24 02:01:57 -0300

    ex-3-d

---
+++ 03-arrays/d.js
@@ -0,0 +1,6 @@
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+months.shift();
+months.pop();
+
+console.log(months);
