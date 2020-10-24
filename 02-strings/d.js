783512189bc9200bc7b7be5e0583a3b2ad815109
Nicolas Andres Calvo <nico.calvo2014@gmail.com>
2020-10-24 00:03:38 -0300

    ex-2-d

---
+++ 02-strings/d.js
@@ -0,0 +1,8 @@
+var stringVar = "Hello World";
+
+subString1 =  stringVar.substring(1,0);
+subString2 =  stringVar.substring(1,11);
+
+subStringComplet = subString1.toUpperCase() + subString2.toLowerCase();
+
+console.log(subStringComplet);
