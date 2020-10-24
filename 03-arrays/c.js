e4641ee7c6ce64efa4d4bcf33a0b5dae051736a9
Nicolas Andres Calvo <nico.calvo2014@gmail.com>
2020-10-24 01:59:13 -0300

    ex-3-c

--- 03-arrays/c.js
+++ 03-arrays/c.js
@@ -1,5 +1,6 @@
 var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
 months.unshift("pre-month");
+months.push("post-month");
 
 console.log(months);
